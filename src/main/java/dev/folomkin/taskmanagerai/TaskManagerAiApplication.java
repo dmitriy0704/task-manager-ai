33072c1edee326f971ea4de42e5080a62aeeb5d0
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2026-06-24 18:44:31 +0300

    init repo

---
+++ src/main/java/dev/folomkin/taskmanagerai/TaskManagerAiApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.taskmanagerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/taskmanagerai/TaskManagerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.taskmanagerai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
